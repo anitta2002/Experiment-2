1bdfd225d054eb0302f3851ca204a9c285e0077a
anitta2002 <98250442+anitta2002@users.noreply.github.com>
2022-01-25 21:04:58 +0530

    Create Lrgst_num.java

---
+++ Lrgst_num.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class lrgst_num
+{
+    public static void main(String args[])
+    {
+        try (Scanner obj = new Scanner(System.in)) {
+            int i;
+            System.out.println("Enter the no.of rows of array");
+            int r=obj.nextInt();
+            System.out.println("Enter the no.of columns of array");
+            int  c=obj.nextInt();
+            int a[][]=new int[r][c];
+            System.out.println("Enter the Elements of array");
+            for(i=0;i<r;i++)
+            {
+                for(int j=0;j<c;j++)
+                {
+                    a[i][j]=obj.nextInt();
+                }
+            }
+            int max=a[0][0];
+            for(i=0;i<r;i++)
+            {
+                for(int j=0;j<c;j++)
+                {
+                    if(a[i][j]>max)
+                    {
+                        max=a[i][j];
+                    }
+                }
+            }
+            System.out.println("Largest number is:"+max);
+        }
+    }
+}
